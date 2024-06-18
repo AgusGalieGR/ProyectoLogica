b01fc6f05887d6b37b3532418880a791ecafcd18
Agustin Galie <144154654+AgusGalieGR@users.noreply.github.com>
2024-06-18 00:10:53 -0300

    Adicion de botones etapa 2

--- src/Game.js
+++ src/Game.js
@@ -162,8 +162,16 @@ if(resultado){
         onClick={(i, j) => handleClick(i, j)}
         
       />
-      <div id="cambio">
-        <button className='button-content'>{statusText}</button> 
+      <div className="button-container">
+        <div id="cambio">
+          <button className='button-content'>{statusText}</button> 
+        </div>
+        <div id="solucion">
+          <button className='button-solution'>Completar</button> 
+        </div>
+        <div id="solucion-especifica">
+          <button className='button-specific-solution'>Pista</button> 
+        </div>
       </div>
       <div>
         <div>{texto}</div> 
